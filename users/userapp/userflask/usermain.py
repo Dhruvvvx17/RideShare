a85ab84739fd0c716a4339409856c6421f94402c
root <root@ip-172-31-34-223.ec2.internal>
2020-04-05 08:53:34 +0000

    docker setup

---
+++ users/userapp/userflask/usermain.py
@@ -0,0 +1,93 @@
+from flask import Flask, jsonify, request, json
+from flask_pymongo import PyMongo
+from flask_restful import Resource, Api
+from bson.objectid import ObjectId
+import requests
+
+app = Flask(__name__)
+#from userflask import app
+api = Api(app)
+
+app.config['MONGO_DBNAME'] = 'users'
+app.config['MONGO_URI'] = 'mongodb://usersdb:27017'
+
+mongo = PyMongo(app)
+
+
+
+class AddUser(Resource):
+    # MAIN API 1 - ADD USER
+    def put(self):
+        username = request.json['username']
+        password = request.json['password']
+        details = {'username' : username, 'password' : password, 'apiNo' : 1}
+        uri = 'http://users:8080/users/DbWrite'
+        dbResponse = requests.post(uri,data=json.dumps(details)).json()
+        return dbResponse
+
+    # TEMP API 1 - LIST ALL USERS
+    def get(self):
+        user = mongo.db.user
+        output = []
+        for q in user.find():
+            output.append({'username':q['username'],'password':q['password']})
+        return {'result':output}
+
+
+
+class RemUser(Resource):
+    # MAIN API 2 - DELETE USER
+    def delete(self,username):
+        user = mongo.db.user
+        details = {'username':username,'apiNo':2}
+        uri = 'http://users:8080/users/DbRead'
+	# users:8080 -> here users is the service 'users'
+        uid = requests.post(uri,data=json.dumps(details)).json()['_id']
+        print(uid)
+        if (uid == -1):
+            output = "User does not exist"
+        else:
+            user.delete_one({'_id':ObjectId(uid)})
+            output = 'Deleted Successfully!'
+        return output
+
+
+
+class DbWrite(Resource):
+    # DB WRITE API 
+    def post(self):
+        user = mongo.db.user 
+        details = request.get_json(force=True)
+        apiNo = details['apiNo']
+        if apiNo == 1:
+            username = details['username']
+            password = details['password']
+            user_id = user.insert({'username' : username, 'password' : password})
+            new_user = user.find_one({'_id' : user_id})
+            output = {'username' : new_user['username'], 'password' : new_user['password']}
+            return jsonify(output)
+
+
+class DbRead(Resource):
+    # DB READ API
+    def post(self):
+        user = mongo.db.user
+        details = request.get_json(force=True)
+        apiNo = details['apiNo']
+        if apiNo == 2:
+            username = details['username']
+            query = user.find_one({'username':username})
+            if not query:
+                resp = {'_id': -1}
+            else:
+                resp = {'_id' : str(query['_id'])}
+            return jsonify(resp)
+
+
+api.add_resource(AddUser,'/users')
+api.add_resource(RemUser,'/users/<username>')
+api.add_resource(DbWrite,'/users/DbWrite')
+api.add_resource(DbRead,'/users/DbRead')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)
--- users/wsgi.py
+++
@@ -1,4 +0,0 @@
-from usersMain import app
-
-if __name__ == "__main__":
-        app.run(port=8080, debug=True)
